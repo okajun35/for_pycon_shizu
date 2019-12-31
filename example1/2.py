8bc27d0a196cdf0e90e2d1dedaada467b4eecd82
岡崎潤 <okazaki@MacBook-Pro-5.local>
2019-12-31 12:24:29 +0900

    examle1にチェックボックスの例を追加

--- example1/2.py
+++ example1/2.py
@@ -1,21 +1,37 @@
 import PySimpleGUI as sg
 
-sg.theme('Dark Green 6')
+#  セクション1 - オプションの設定と標準レイアウト
+sg.theme('Dark Blue 3')
 
-layout = [[sg.Text('Please click the button', key='-TEXT-')],
-          [sg.Submit(button_text='実行ボタン')]]
+layout = [
+    [sg.Text('Python GUI')],
+    [sg.Text('名前', size=(15, 1)), sg.InputText('○○〇×××')],
+    [sg.Text('住所', size=(15, 1)), sg.InputText('△△△△村')],
+    [sg.Text('電話番号', size=(15, 1)), sg.InputText('xxx-xxx-xxx')],
+    [sg.Checkbox('同意する', default=True)],
+    [sg.Submit(button_text='実行ボタン')]
+]
 
-# ウィンサイズはsizeに(縦の大きさ,横の大きさ)で記載
-window = sg.Window(title='show input text', size=(200, 100)).Layout(layout)
+# セクション 2 - ウィンドウの生成
+window = sg.Window('住所を入力', layout)
 
-# イベントループ
+# セクション 3 - イベントループ
 while True:
     event, values = window.read()
-    if event is None:
-        break
-    if event == '実行ボタン':
-        update_text = "Button clicked."
-        # 表示内容を更新する際はウィジェットに指定されたkeyの値に.Update("文字列")を入れることで可能
-        window.Element('-TEXT-').Update(update_text)
 
+    if event is None:
+        print('exit')
+        break
+
+    if event == '実行ボタン':
+        show_message = "名前：" + values[0] + 'が入力されました。\n'
+        show_message += "住所：" + values[1] + 'が入力されました。\n'
+        show_message += "電話番号：" + values[2] + "が入力されました。"
+        show_message += "同意のチェックは：" + str(values[3]) + "です"
+        print(show_message)
+
+        # ポップアップ
+        sg.Popup(show_message)
+
+# セクション 4 - ウィンドウの破棄と終了
 window.close()
--- example1/3.py
+++ example1/3.py
@@ -1,48 +1,21 @@
 import PySimpleGUI as sg
 
-#  セクション1 - オプションの設定と標準レイアウト
-sg.theme('Dark Blue 3')
+sg.theme('Dark Green 6')
 
-frame1 = sg.Frame('住所', [[sg.Text('郵便番号', size=(15, 1))],
-                         [sg.InputText(key='-POST-NUM-')],
-                         [sg.Text('都道府県', size=(15, 1), )],
-                         [sg.InputText(key='-PREFECTURES-')],
-                         [sg.Text('住所', size=(15, 1))],
-                         [sg.InputText(key='-ADDRESS-')],
-                         ],
-                  relief=sg.RELIEF_SUNKEN, tooltip='住所をいれてね！')
+layout = [[sg.Text('Please click the button', key='-TEXT-')],
+          [sg.Submit(button_text='実行ボタン')]]
 
-layout = [
-    [sg.Text('Python GUI')],
-    [sg.Text('名前', size=(15, 1)), sg.InputText(
-        default_text='○○〇×××', key='-USER-NAME-')],
-    [sg.Text('住所を入れてね', size=(15, 1)), frame1],  # frame1のレイアウトを入れ子にして入れている
-    [sg.Text('電話番号', size=(15, 1)), sg.InputText(
-        default_text='xxx-xxx-xxx', key='-PHONE-NUM-')],
-    [sg.Button('実行ボタン')]
-]
+# ウィンサイズはsizeに(縦の大きさ,横の大きさ)で記載
+window = sg.Window(title='show input text', size=(200, 100)).Layout(layout)
 
-# セクション 2 - ウィンドウの生成
-window = sg.Window('住所を入力', layout)
-
-# セクション 3 - イベントループ
+# イベントループ
 while True:
     event, values = window.read()
-
     if event is None:
-        print('exit')
         break
-
     if event == '実行ボタン':
-        print(values)
-        show_message = "名前：" + values['-USER-NAME-'] + 'が入力されました。\n'
-        show_message += "郵便番号：" + values['-POST-NUM-'] + 'が入力されました。\n'
-        show_message += "都道府県：" + values['-PREFECTURES-'] + 'が入力されました。\n'
-        show_message += "住所：" + values['-ADDRESS-'] + 'が入力されました。\n'
-        show_message += "電話番号：" + values['-PHONE-NUM-'] + "が入力されました。"
+        update_text = "Button clicked."
+        # 表示内容を更新する際はウィジェットに指定されたkeyの値に.update("文字列")を入れることで可能
+        window['-TEXT-'].update(update_text)
 
-        # ポップアップ
-        sg.popup(show_message)
-
-# セクション 4 - ウィンドウの破棄と終了
 window.close()
